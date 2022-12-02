361dc36c77891b921e9d144f56fe7d36914deaa0
Oleg <9helgi9gmail.com>
2022-12-02 15:48:23 +0100

    useEffect clean

--- src/stories/useEffect.stories.tsx
+++ src/stories/useEffect.stories.tsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from "react";
 
 
+
+
 export default {
     title: 'useEffect demo'
 }
 
 
-
-export const UseEffect= () => {
+export const UseEffect = () => {
     console.log('useEffect')
     const [counter, setCounter] = useState(1)
     const [fake, setFake] = useState(1)
@@ -23,24 +24,25 @@ export const UseEffect= () => {
     useEffect(() => {
         console.log('useEffect first ')
         document.title = counter + ''
-    },[])
+    }, [])
     useEffect(() => {
         console.log('useEffect counter changes ')
         document.title = counter + ''
 
-    },[counter])
+    }, [counter])
 
     useEffect(() => {
         window.setInterval(() => {
             let b = new Date()
             let c = b.getHours() + ':' + b.getMinutes() + ':' + b.getSeconds()
             setTime(c)
-        },1000)
-    },[time])
+        }, 1000)
+
+    }, [time])
 
     return <div>
-        <button onClick={() =>setCounter(state=> state +1 )}>counter+</button>
-        <button onClick={() =>setFake(fake=> fake +1 )}>fake+</button>
+        <button onClick={() => setCounter(state => state + 1)}>counter+</button>
+        <button onClick={() => setFake(fake => fake + 1)}>fake+</button>
         {counter}
         {fake}
         <br/>
@@ -49,5 +51,21 @@ export const UseEffect= () => {
     </div>
 }
 
+export const SetIntervalExample = () => {
+    const [int, setInt] = useState(0)
+    console.log('Component rendered with :' + int)
 
+    useEffect(() => {
+        const id = setInterval(() => {
+            setInt(int + 1)
+            console.log('3 second passed')
+        },3000)
+        return () => {
+            clearInterval(id)
+        }
 
+    },[int])
+    return <div>
+        count: {int}
+    </div>
+}
